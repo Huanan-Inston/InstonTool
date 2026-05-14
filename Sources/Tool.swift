5abe0652a5f2434673f6dd471d9a65a2c5fc495f
Huanan <donghuanan@inston.ltd>
2026-05-14 19:29:38 +0800

    update help

--- Sources/Tool.swift
+++ Sources/Tool.swift
@@ -14,16 +14,16 @@ struct Tool: AsyncParsableCommand {
         abstract: "A Tool For Lazy Operations",
         usage: """
         tool <SUBCOMMAND>
-        tool auth --token <TOKEN> --secret <SECRET>
+        tool auth --access_key <TOKEN> --access_secret <SECRET>
         tool setup "(?<=L\\(@\")([_\\w]+)(?=\")" --strings ./MapRunner/Strings
         tool localize --strings ./MapRunner/Strings --keys having_problem_tell_gpt --keys good_job
         tool localize --strings ./MapRunner/Strings --keys-file ./keys.txt
-        tool localize --strings ./MapRunner/Strings
+        tool localize --strings ./MapRunner/Strings --downloaded <dowload path>
         tool generate ./LangGen/OCL+Keys.h.template ./LangGen/OCL+Keys.m.template --output ./LangGen --strings ./MapRunner/Strings    
 
         Recommand [mint](github.com/yonaskolb/Mint) for better experience.
         example:
-          - mint run Huanan-Inston/InstonTool auth --token <TOKEN> --secret <SECRET>
+          - mint run Huanan-Inston/InstonTool auth --access_key <TOKEN> --access_secret <SECRET>
           - mint run Huanan-Inston/InstonTool localize --strings ./MapRunner/Strings --keys having_problem_tell_gpt --keys good_job
           - mint run Huanan-Inston/InstonTool localize --strings ./MapRunner/Strings
         """,
